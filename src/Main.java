6b5c2e2e772dc8780818b55f0bbeab9d89a84e55
Lucas Meireles <meireles31@msn.com>
2017-05-29 23:10:11 -0300

    Removed permutationgenerator from main

--- src/Main.java
+++ src/Main.java
@@ -49,13 +49,9 @@ public class Main {
 
         cypherText = cypherText.toUpperCase();
 
-        System.out.println(PermutationGenerator.GenerateAllPermutationsOfLength(1));
-        System.out.println(PermutationGenerator.GenerateAllPermutationsOfLength(2));
-
-        System.out.println(PermutationGenerator.GenerateAllPermutationsOfLength(3));
-
         int keyLength = Kasiski.test(cypherText);
         System.out.println(keyLength);
+
         String keyEstimate = PermutationGenerator.getMostLikelyKey(cypherText, keyLength);
         System.out.println(keyEstimate);
 
@@ -63,10 +59,7 @@ public class Main {
         Brute fakeSolution = new Brute(fakeKey, cypherText);
         fakeSolution.run();
 
-        //ArrayList<String> allPossibleKeys = PermutationGenerator.GenerateAllPermutationsOfLength(keyLength);
-        //for(String currentKey : allPossibleKeys){
-        //    Brute worker = new Brute(currentKey, cypherText);
-        //    worker.run();
-        //}
+        Brute realSolution = new Brute(keyEstimate, cypherText);
+        realSolution.run();
     }
 }
