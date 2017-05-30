05fde37650257fee5e2fdc675bf6aa4dc1a99da4
Victor Villas <villasv@outlook.com>
2017-05-29 21:42:42 -0300

    kasiski almost done

--- src/Kasiski.java
+++ src/Kasiski.java
@@ -1,26 +1,56 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Kasiski {
+
     public static int test(String cypherText) {
-        HashMap<String, ArrayList<Integer>> substringMap = new HashMap<>();
-        String substring;
+        HashMap<String, ArrayList<Integer>> substringPositions = new HashMap<>();
 
         // count trigrams
         for (int i = 0; i < cypherText.length()-2; i++) {
-            substring = cypherText.substring(i, i+3);
+            String substring = cypherText.substring(i, i+3);
 
-            if (!substringMap.containsKey(substring)) {
-                substringMap.put(substring, new ArrayList<>());
-            }
+            if (!substringPositions.containsKey(substring))
+                substringPositions.put(substring, new ArrayList<>());
 
-            substringMap.get(substring).add(i);
+            substringPositions.get(substring).add(i);
         }
 
         // discard unique trigrams
+        HashMap<String, ArrayList<Integer>> repeatedPositions = new HashMap<>(substringPositions);
+        for (String substring : substringPositions.keySet())
+            if (repeatedPositions.get(substring).size() < 2)
+                repeatedPositions.remove(substring);
 
+        // get the prime factors of the differences
+        HashMap<Integer, Integer> factorCounts = new HashMap<>();
+        for (String substring : repeatedPositions.keySet()) {
+            ArrayList<Integer> diffs = getDifferences(repeatedPositions.get(substring));
+            for (Integer diff : diffs) {
 
+            }
+        }
+
+        // estimate the key size
+        Integer bestKeySize = null, maxFrequency = 0;
+        for (Integer keySize : factorCounts.keySet()) {
+            Integer keyFrequency = factorCounts.get(keySize);
+            if (keyFrequency > maxFrequency && keySize > 2 && keySize < 15) {
+                bestKeySize = keySize;
+                maxFrequency = keyFrequency;
+            }
+        }
 
         return 13;
+    }
+
+    private static ArrayList<Integer> getDifferences(ArrayList<Integer> values) {
+        ArrayList<Integer> diffs = new ArrayList<>();
+
+        for (int i = 1; i < values.size(); i++)
+            diffs.add(values.get(i)-values.get(i-1));
+
+        return diffs;
     }
 }
